7d12d207c5f1d6a356e82de8ea4e33ece741ee50
meow@lianj.com <123456789>
2018-02-06 13:43:12 +0800

    update 增加get

--- src/index.js
+++ src/index.js
@@ -1,45 +1,65 @@
 function createWatchers(){
-  return Object.keys(this.$options.asyncComputed).forEach(key => {
-    this.$asyncWatchers = this.$asyncWatchers || [];
-  if (!this.$asyncWatchers[key]) {
-    this.$asyncWatchers[key] = this.$watch(() => this.$options.asyncComputed[key].call(this, this.$asyncOptions.context.call(this)), async (p) => {
-      this[key] = undefined;
-      this[key] = await p;
-    }, {
-      immediate: true
-    });
-  }
-});
+  return this.$asyncWatcherItems.forEach(item => {
+    const key = item.key
+    if (!item.watcher) {
+      item.watcher = this.$watch(() => item.get.call(this, this.$asyncOptions.context.call(this)), async (p) => {
+        this[key] = typeof item.default === 'function' ? item.default.call(this, this[key]) : item.default;
+        this[key] = await p;
+      }, {
+        immediate: true
+      });
+    }
+  });
 }
 
 function destroyWatchers() {
-  return Object.keys(this.$options.asyncComputed).forEach(key => {
-      this.$asyncWatchers[key]();
+  return this.$asyncWatcherItems.forEach(item => {
+    item.watcher();
+    item.watcher = null;
   })
 }
 
 export const mixin = {
   activated() {
-    if (this.$options.asyncComputed) {
+    if (this.$asyncWatcherItems) {
       createWatchers.call(this);
     }
   },
   deactivated(){
-    if (this.$options.asyncComputed) {
+    if (this.$asyncWatcherItems) {
       destroyWatchers.call(this);
     }
   },
   created() {
-    if (this.$options.asyncComputed) {
+    if (this.$asyncWatcherItems) {
       createWatchers.call(this);
     }
   },
   data() {
     if (this.$options.asyncComputed) {
-      return Object.keys(this.$options.asyncComputed).reduce((data, key) => {
-        data[key] = undefined;
+      const DEFAULT_ITEM = {
+        default: undefined,
+        watcher: null
+      }
+      this.$asyncWatcherItems = Object.keys(this.$options.asyncComputed).map((key) => {
+        let item = this.$options.asyncComputed[key];
+        if (typeof item !== 'object') {
+          item = {
+            get: item
+          }
+        }
+        return {
+          key,
+          ...DEFAULT_ITEM,
+          ...item
+        }
+      });
+    }
+    if (this.$asyncWatcherItems) {
+      return this.$asyncWatcherItems.reduce((data, item) => {
+        data[item.key] = typeof item.default === 'function' ? item.default.call(this) : item.default;
         return data;
-      }, {});
+      }, {})
     }
     return {};
   }
@@ -52,4 +72,3 @@ export default {
     Vue.mixin(mixin);
   }
 }
-
